f13d796b2133d96b494974f8c920e32de7e3fd96
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-23 09:19:25 +0900

    Initial commit

---
+++ src/jeff/balbalosa/Car.java
@@ -0,0 +1,2 @@
+package jeff.balbalosa;public class Car {
+}
---
+++ src/jeff/balbalosa/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("OOP Polymorphism Challenge");
+        System.out.println("-----------------------------------");
+    }
+}
